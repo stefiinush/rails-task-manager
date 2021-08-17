c32d3fe9f665a91a7513bb9d1047e2d6950984d1
Stefania Ella Centea <stefiinusj@gmail.com>
2021-08-17 17:48:20 +0100

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # to read all tasks
+  get "tasks", to: "tasks#index"
+  # to add a new task
+  get "tasks/new" ,to: "tasks#new" #, as: :new_task
+  # to post the new task
+  post "tasks", to: "tasks#create"
+  # to read one task
+  get "tasks/:id", to: "tasks#show", as: :task
+  # to edit a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # to update a task
+  patch "tasks/:id", to: "tasks#update"
+  # to delete a task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
 end
